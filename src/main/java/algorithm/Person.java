013e62233253af3a447f2a44ffea13c3427be984
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2022-06-07 10:57:58 -0700

    Remove dead code in Person class

--- src/main/java/algorithm/Person.java
+++ src/main/java/algorithm/Person.java
@@ -11,19 +11,4 @@ public class Person {
 		this.birthDate = birthDate;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Calendar getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(Calendar birthDate) {
-		this.birthDate = birthDate;
-	}
 }
