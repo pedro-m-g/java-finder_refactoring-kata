536baca542c2db06f40cc39e00b3139d7d8fad30
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2022-06-07 11:12:05 -0700

    Refactor: Extract people set product to method in Finder class

--- src/main/java/algorithm/Finder.java
+++ src/main/java/algorithm/Finder.java
@@ -11,29 +11,14 @@ public class Finder {
 	}
 
 	public F Find(FT ft) {
-		List<F> tr = new ArrayList<F>();
+		List<F> peopleSetProduct = peopleSetProduct();
 
-		for (int i = 0; i < people.size() - 1; i++) {
-			for (int j = i + 1; j < people.size(); j++) {
-				F r = new F();
-				if (people.get(i).birthDate().getTimeInMillis() < people.get(j).birthDate().getTimeInMillis()) {
-					r.P1 = people.get(i);
-					r.P2 = people.get(j);
-				} else {
-					r.P1 = people.get(j);
-					r.P2 = people.get(i);
-				}
-				r.D = r.P2.birthDate().getTimeInMillis() - r.P1.birthDate().getTimeInMillis();
-				tr.add(r);
-			}
-		}
-
-		if (tr.size() < 1) {
+		if (peopleSetProduct.size() < 1) {
 			return new F();
 		}
 
-		F answer = tr.get(0);
-		for (F result : tr) {
+		F answer = peopleSetProduct.get(0);
+		for (F result : peopleSetProduct) {
 			switch (ft) {
 				case One:
 					if (result.D < answer.D) {
@@ -50,5 +35,25 @@ public class Finder {
 		}
 
 		return answer;
+	}
+
+	private List<F> peopleSetProduct() {
+		List<F> tr = new ArrayList<F>();
+
+		for (int i = 0; i < people.size() - 1; i++) {
+			for (int j = i + 1; j < people.size(); j++) {
+				F r = new F();
+				if (people.get(i).birthDate().getTimeInMillis() < people.get(j).birthDate().getTimeInMillis()) {
+					r.P1 = people.get(i);
+					r.P2 = people.get(j);
+				} else {
+					r.P1 = people.get(j);
+					r.P2 = people.get(i);
+				}
+				r.D = r.P2.birthDate().getTimeInMillis() - r.P1.birthDate().getTimeInMillis();
+				tr.add(r);
+			}
+		}
+		return tr;
 	}
 }
