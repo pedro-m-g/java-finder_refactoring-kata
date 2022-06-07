659b709e8f85bad53ea253f46c098208483db704
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2022-06-07 12:16:53 -0700

    Refactor: Remove dead code in PersonPair class

--- src/main/java/algorithm/PersonPair.java
+++ src/main/java/algorithm/PersonPair.java
@@ -17,24 +17,12 @@ public class PersonPair {
 		return this.younger;
 	}
 
-	public void younger(Person younger) {
-		this.younger = younger;
-	}
-
 	public Person older() {
 		return this.older;
 	}
 
-	public void older(Person older) {
-		this.older = older;
-	}
-
 	public long ageDiff() {
 		return this.ageDiff;
-	}
-
-	public void ageDiff(long ageDiff) {
-		this.ageDiff = ageDiff;
 	}
 
 }
